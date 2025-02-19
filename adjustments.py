aa5ad510fd2373cd8e312924835866f942dbc324
kostaaaaaa <deanfountas@gmail.com>
2025-02-19 16:49:55 -0500

    Lieb

--- adjustments.py
+++ adjustments.py
@@ -2,7 +2,7 @@ from Lattice_class import Lattice
 import numpy as np
 
 v = [[1,0],[0,1]]
-b=[[0,0],[1/2,0],[0,1/2]]
+b =[[0,0],[1/2,0],[0,1/2]]
 
 sq = Lattice(1, v, b, 10)
 sq.plot_lattice_superlattice(n=2)
